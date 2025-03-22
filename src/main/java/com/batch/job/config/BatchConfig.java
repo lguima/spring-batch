2a2cfb03750fc4907b63e2596cf6e85b034da091
Lucas Guima <lucasguima.felipe@gmail.com>
2025-03-22 18:51:09 -0300

    set up resourceless job repository

--- src/main/java/com/batch/job/config/BatchConfig.java
+++ src/main/java/com/batch/job/config/BatchConfig.java
@@ -1,41 +1,33 @@
 package com.batch.job.config;
 
-import com.batch.job.job.FileDeletingTasklet;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
-import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.TaskExecutorJobLauncher;
 import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.repository.support.ResourcelessJobRepository;
+import org.springframework.batch.support.transaction.ResourcelessTransactionManager;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.integration.redis.util.RedisLockRegistry;
-import org.springframework.integration.support.locks.DefaultLockRegistry;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class BatchConfig {
   @Bean
-  public Job taskletJob(JobRepository jobRepository, Step step) {
-    return new JobBuilder("taskletJob", jobRepository)
-      .start(step)
-      .build();
+  public PlatformTransactionManager transactionManager() {
+    return new ResourcelessTransactionManager();
   }
 
   @Bean
-  public Step deleteFilesInDir(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
-    return new StepBuilder("deleteFilesInDir", jobRepository)
-      .tasklet(fileDeletingTasklet(), transactionManager)
-      .build();
+  public JobRepository jobRepository() {
+    return new ResourcelessJobRepository();
   }
 
   @Bean
-  public FileDeletingTasklet fileDeletingTasklet() {
-    FileDeletingTasklet tasklet = new FileDeletingTasklet();
-
-    tasklet.setDirectoryResource(new FileSystemResource("target/test-outputs/test-dir"));
-
-    return tasklet;
+  public JobLauncher jobLauncher(JobRepository jobRepository) {
+    TaskExecutorJobLauncher jobLauncher = new TaskExecutorJobLauncher();
+    jobLauncher.setJobRepository(jobRepository);
+    return jobLauncher;
   }
 }
