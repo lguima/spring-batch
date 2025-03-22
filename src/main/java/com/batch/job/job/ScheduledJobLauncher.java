93f3b9236a4cc1d4508e9266edb94eb3a8440eaa
Lucas Guima <lucasguima.felipe@gmail.com>
2025-03-22 18:52:29 -0300

    apply the distributed lock to the job launcher

--- src/main/java/com/batch/job/job/ScheduledJobLauncher.java
+++ src/main/java/com/batch/job/job/ScheduledJobLauncher.java
@@ -1,15 +1,21 @@
 package com.batch.job.job;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.support.locks.ExpirableLockRegistry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+
 @Configuration
 @EnableScheduling
+@Slf4j
 public class ScheduledJobLauncher {
   @Autowired
   JobLauncher jobLauncher;
@@ -17,8 +23,25 @@ public class ScheduledJobLauncher {
   @Autowired
   Job job;
 
+  @Autowired
+  private ExpirableLockRegistry lockRegistry;
+
   @Scheduled(cron = "${job.scheduling.cron}")
   public void launchJob() throws Exception {
-    jobLauncher.run(job, new JobParameters());
+    Lock lock = lockRegistry.obtain("lock00");
+    log.debug("Lock: " + lock.toString());
+
+    boolean lockAcquired =  lock.tryLock(1, TimeUnit.SECONDS);
+    log.debug("Lock Acquired: " + lockAcquired);
+
+    if (!lockAcquired) {
+      throw new Exception("Lock not acquired");
+    }
+
+    try {
+      jobLauncher.run(job, new JobParameters());
+    } finally {
+      lock.unlock();
+    }
   }
 }
